4175a1ae3ec5b1f6f71ec7f81598b8932b8de6fc
skv-analyst <skv.inet@gmail.com>
2025-03-17 10:21:32 +0300

    add new solutions

--- leetcode/09-hashtable.py
+++ leetcode/09-hashtable.py
@@ -52,5 +52,38 @@ def solution_3(s):
 
     return min((idx for idx in db.values() if idx is not None), default=-1)
 
+
 for i, v in enumerate(examples):
     print(f"Example: {i + 1}. Values: {v}. Result: {solution_3(v)}")
+
+# %% 1748. Sum of Unique Elements
+from collections import Counter
+examples = [
+    [1, 2, 3, 2],  # Output: 4
+    [1, 1, 1, 1, 1],  # Output: 0
+    [1, 2, 3, 4, 5]  # Output: 15
+]
+
+
+def solution_1(nums):
+    # Time: O(n)
+    # Space: O(n)
+    db = {}
+    for num in nums:
+        if num not in db:
+            db[num] = 1
+        else:
+            db[num] += 1
+
+    return sum([num for num, cnt in db.items() if cnt == 1])
+
+
+def solution_2(nums):
+    # Time: O(n)
+    # Space: O(n)
+    counter = Counter(nums)
+    return sum([num for num, cnt in counter.items() if cnt == 1])
+
+
+for i, v in enumerate(examples):
+    print(f"Example: {i + 1}. Values: {v}. Result: {solution_2(v)}")
