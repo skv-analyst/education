27c31b8e7fb74e4f79a841e7266e880ce9d48560
skv-analyst <skv.inet@gmail.com>
2025-04-07 10:07:53 +0300

    add new solutions

--- leetcode/11-binary-search.py
+++ leetcode/11-binary-search.py
@@ -1,3 +1,6 @@
+from string import ascii_lowercase
+
+
 def show_result(examples, solution, is_dict=None):
     for i, v in enumerate(examples):
         if is_dict:
@@ -122,6 +125,9 @@ examples_2529 = [
 
 
 def solution_2529(nums):
+    # Time: O(log n)
+    # Space: O(1)
+
     # negative
     l, r = 0, len(nums) - 1
     while l <= r:
@@ -146,9 +152,83 @@ def solution_2529(nums):
 
     return max(neg, pos)
 
+
+# 367. Valid Perfect Square
+examples_367 = [16, 14]
+
+
+def solution_367(num):
+    # Time: O(log n)
+    # Space: O(1)
+
+    l, r = 0, num
+
+    while l <= r:
+        mid = (l + r) // 2
+
+        if mid * mid == num:
+            return True
+
+        if mid * mid > num:
+            r = mid - 1
+        else:
+            l = mid + 1
+
+    return False
+
+
+# 69. Sqrt(x)
+examples_69 = [4, 8]
+
+
+def solution_69(x):
+    # Time: O(log n)
+    # Space: O(1)
+
+    l, r = 0, x
+
+    while l <= r:
+        mid = (l + r) // 2
+
+        if mid * mid == x:
+            return mid
+
+        if mid * mid > x:
+            r = mid - 1
+        else:
+            l = mid + 1
+
+    return r
+
+
+# 744. Find Smallest Letter Greater Than Target
+examples_744 = [
+    {"letters": ["c", "f", "j"], "target": "a"},
+    {"letters": ["c", "f", "j"], "target": "c"},
+    {"letters": ["x", "x", "y", "y"], "target": "z"},
+    {"letters": ["c", "f", "j"], "target": "d"}
+]
+
+
+def solution_744(letters, target):
+    l, r = 0, len(letters) - 1
+    while l <= r:
+        mid = (l + r) // 2
+
+        if letters[mid] <= target:
+            l = mid + 1
+        else:
+            r = mid - 1
+
+    return letters[l] if l < len(letters) else letters[0]
+
+
 if __name__ == '__main__':
     # show_result(examples_704, solution_704, is_dict=True)
     # show_result(examples_374, solution_374)
     # show_result(examples_278, solution_278)
     # show_result(examples_35, solution_35, is_dict=True)
-    show_result(examples_2529, solution_2529)
+    # show_result(examples_2529, solution_2529)
+    # show_result(examples_367, solution_367)
+    # show_result(examples_69, solution_69)
+    show_result(examples_744, solution_744, is_dict=True)
