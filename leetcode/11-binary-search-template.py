7e2cc82b6a2d491a789a490c706d706e15999da1
skv-analyst <skv.inet@gmail.com>
2025-04-07 22:36:13 +0300

    add new solutions

--- leetcode/11-binary-search-template.py
+++ leetcode/11-binary-search-template.py
@@ -17,7 +17,7 @@ def binary_search(nums, target):
     return ans
 
 
-def lower_bound(nums, target):
+def find_first(nums, target):
     l, r = 0, len(nums) - 1
     ans = None
 
@@ -27,6 +27,41 @@ def lower_bound(nums, target):
         if target == nums[mid]:
             ans = mid
             r = mid - 1
+
+        elif target < nums[mid]:
+            r = mid - 1
+
+        elif target > nums[mid]:
+            l = mid + 1
+
+    return ans
+
+
+def lower_bound(nums, target):
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        mid = l + (r - l) // 2
+
+        if target <= nums[mid]:
+            r = mid - 1
+
+        elif target > nums[mid]:
+            l = mid + 1
+
+    return l
+
+
+def find_last(nums, target):
+    l, r = 0, len(nums) - 1
+    ans = None
+
+    while l <= r:
+        mid = l + (r - l) // 2
+
+        if target == nums[mid]:
+            ans = mid
+            l = mid + 1
 
         elif target < nums[mid]:
             r = mid - 1
@@ -39,20 +74,14 @@ def lower_bound(nums, target):
 
 def upper_bound(nums, target):
     l, r = 0, len(nums) - 1
-    ans = None
 
     while l <= r:
         mid = l + (r - l) // 2
 
-        if target == nums[mid]:
-            ans = mid
-            l = mid + 1
-
-        elif target < nums[mid]:
+        if target < nums[mid]:
             r = mid - 1
 
-        elif target > nums[mid]:
+        elif target >= nums[mid]:
             l = mid + 1
 
-    return ans
-
+    return l
